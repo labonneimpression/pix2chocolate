326017f8e766da4a9130d859d4b2c8c7e12bccfc
myselfhimself <jonathan.schroder@gmail.com>
2019-10-30 09:24:15 +0100

    introducing python-based alternative to sh script, with proper help doc and executables detection

---
+++ pix2chocolate.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import argparse
+import shutil
+import subprocess
+import sys
+import logging
+
+DEFAULT_INPUT_IMAGE = 'LaBonneImpressionLogoHeightmap.svg'
+DEFAULT_DISPLACEMENT_TEXTURE_IMAGE = 'LaBonneImpressionLogoHeightmap.png' #todo more generic filename
+DEFAULT_RENDERING_IMAGE = 'test0001.png'
+
+parser = argparse.ArgumentParser(description='Picture to chocolate renderer')
+parser.add_argument('image', nargs='?', type=str, default=DEFAULT_INPUT_IMAGE,
+                    help='image to render onto chocolate (default: %(default)s)')
+parser.add_argument('--verbose', '-v', action='count', default=1)
+
+args = parser.parse_args()
+args.verbose = 40 - (10*args.verbose) if args.verbose > 0 else 0
+logging.basicConfig(level=args.verbose, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+if shutil.which("convert"):
+    exitcode, output = subprocess.getstatusoutput('convert -rotate "90>" -background "#000000" -density 900 {input_file} {output_file}'.format(input_file=args.image, output_file=DEFAULT_DISPLACEMENT_TEXTURE_IMAGE))
+    logging.info(output)
+else:
+    print('No prior use of ImageMagick''s convert: process not found in path.')
+
+if shutil.which("blender"):
+    exitcode, output = subprocess.getstatusoutput(
+        'blender -b micro_displacement_test.blend -x 1 -o test -f 1')
+    logging.info(output)
+else:
+    print('FATAL ERROR: Blender not found!!!')
+    sys.exit(1)
+
+if shutil.which("xdg-open"):
+    exitcode, output = subprocess.getstatusoutput('xdg-open ' + DEFAULT_RENDERING_IMAGE)
+    logging.info(output)
