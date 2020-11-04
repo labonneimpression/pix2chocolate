09a7844a393fdcdd511488a1dbb2656b53f4d746
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-11-04 01:27:36 +0100

    #5 svg generation (trace)

--- cli_example_multiletters.py
+++ cli_example_multiletters.py
@@ -2,7 +2,9 @@ import os
 import os.path
 import shutil
 from xml.dom.minidom import parse
+import subprocess
 import sys
+
 import fire
 
 sys.path.append("cli/")
@@ -11,7 +13,7 @@ from gmic import Gmic
 G = Gmic()
 DEFAULT_OUTPUT_DIR="generated"
 
-def render_chocolate_biscuits_for_text(text="JOYEUX ANNIVERSAIRE",output_dir=DEFAULT_OUTPUT_DIR,gmic_command=""):
+def render_chocolate_biscuits_for_text(text="JOYEUX ANNIVERSAIRE",output_dir=DEFAULT_OUTPUT_DIR,gmic_command="",vectorize=True):
     SVG_TO_EDIT = "singleLetterHeightmap.svg"
     TEMP_SVG = "tmp.svg"
     dom = parse(SVG_TO_EDIT)
@@ -26,11 +28,37 @@ def render_chocolate_biscuits_for_text(text="JOYEUX ANNIVERSAIRE",output_dir=DEF
         original_filename = "letters0001.png"
         final_filename = "letter" + str(letter_id).zfill(4) + ".png"
         final_gmic_filename = "letter_gmic" + str(letter_id).zfill(4) + ".png"
+        final_vector_filename = "letter" + str(letter_id).zfill(4) + ".svg"
         if gmic_command:
-            G.run("{} {} output {}".format(original_filename, gmic_command, final_gmic_filename))
+            _gmic_command = "{} {} output {}".format(original_filename, gmic_command, final_gmic_filename)
+            print("gmic pass -- {}".format(_gmic_command))
+            G.run(_gmic_command)
+
+        if vectorize:
+            # imitating convert + potrace Gist at https://gist.github.com/ykarikos/2892009
+            # mkbitmap -x -t 0.5 -f 3 letter0001.pnm -o mk.pbm; xdg-open mk.pbm ; potrace -s -o mk.svg mk.pbm ; xdg-open mk.svg
+            # reference help: http://potrace.sourceforge.net/mkbitmap.html
+            pnm_filename = final_vector_filename.replace(".svg", ".pnm")
+            pbm_filename = final_vector_filename.replace(".svg", ".pbm")
+            _gmic_command = "{} output {}".format(final_gmic_filename if gmic_command else original_filename, final_vector_filename.replace(".svg", ".pnm"))
+            print("gmic png->pnm pass -- {}".format(_gmic_command))
+            G.run(_gmic_command)
+
+            _mkbitmap_command = "mkbitmap -x -t 0.5 -f 3 {} -o {}".format(pnm_filename, pbm_filename)
+            print("mkbitmap pnm->pbm pass -- {}".format(_mkbitmap_command))
+            exitcode, output = subprocess.getstatusoutput(_mkbitmap_command)
+
+            potrace_command = "potrace -s -o {} {}".format(final_vector_filename, pbm_filename)
+            print("potrace vectorization -- {}".format(potrace_command))
+            exitcode, output = subprocess.getstatusoutput(potrace_command)
+            shutil.move(pnm_filename, os.path.join(output_dir, pnm_filename))
+            shutil.move(pbm_filename, os.path.join(output_dir, pbm_filename))
+            shutil.move(final_vector_filename, os.path.join(output_dir, final_vector_filename))
+
+        if os.path.exists(final_gmic_filename):
             shutil.move(final_gmic_filename, os.path.join(output_dir, final_gmic_filename))
         shutil.move(original_filename, os.path.join(output_dir, final_filename))
-        print("{} -> {}".format(letter, final_filename))
+        print("{} -> {}".format(letter, os.path.join(output_dir, final_filename)))
         print("done pix2chocolate")
         letter_id += 1
         os.unlink(TEMP_SVG)
